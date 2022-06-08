6ee1b77678ea34acedb89fa6c97bf60fd20f5296
Trailblazer-Yoo <yousun9591@naver.com>
2022-06-08 09:12:07 +0900

    2022-06-08

--- src/mongoDB/test.py
+++ src/mongoDB/test.py
@@ -1,19 +1,14 @@
-import schedule
-import time
+from pymongo import MongoClient
 
-# step2.실행할 함수 선언
-class Interest_Crawling():
-    def __init__(self):
-        host = '35.78.27.97'
-        port = '27017'
-    def message(self):
-        print("스케쥴 실행중...")
-
-
-# step3.실행 주기 설정
-schedule.every().monday.at("17:00").do(lambda: Interest_Crawling().message())
-
-# step4.스캐쥴 시작
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+client = MongoClient('localhost', 27017)
+host = '35.78.27.97'
+port = '27017'
+client_aws = MongoClient(f'mongodb://{host}:{port}')  # mongoDB는 내 컴퓨터 내의 27017 포트로 들어가서 DB 와 연결해준다.
+for d in ['exchange', 'interest']:
+    db = client[d]
+    db_aws = client_aws[d]
+    for country in db.list_collection_names():
+        coll = db[country]
+        coll_aws = db_aws[country]
+        data = list(coll.find({}, {'_id' : 0}))
+        coll_aws.insert_many(data)
